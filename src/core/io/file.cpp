b60c0a622e59964c88e4190dcc26695eae00f1a1
Madotsuki <madotsuki@redchan.it>
2015-03-18 11:54:03 -0500

    License update, resuming work.

--- src/core/io/file.cpp
+++ src/core/io/file.cpp
@@ -1,20 +1,23 @@
 /*
-Copyright (C) 2015 <Madotsuki>
+Copyright © 2015 Madotsuki <madotsuki@redchan.it>
 
-This file is part of Breaker Engine.
+This software is provided 'as-is', without any express or implied warranty. In
+no event will the authors be held liable for any damages arising from the use of
+this software.
 
-Breaker Engine is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Permission is granted to anyone to use this software for any purpose, including
+commercial applications, and to alter it and redistribute it freely, subject to
+the following restrictions:
 
-Breaker Engine is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+	1.	The origin of this software must not be misrepresented; you must not
+		claim that you wrote the original software. If you use this software in
+		a product, an acknowledgment in the product documentation would be
+		appreciated but is not required.
 
-You should have received a copy of the GNU Affero General Public License
-along with Breaker Engine.  If not, see <http://www.gnu.org/licenses/>.
+	2.	Altered source versions must be plainly marked as such, and must not be
+		misrepresented as being the original software.
+
+	3. This notice may not be removed or altered from any source distribution.
 */
 
 #include "file.h"
